fea437d47d618a17f6c94315fa07e57e9ef2e235
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-06-26 16:23:51 +0200

    vue2

--- vue-products/src/App.vue
+++ vue-products/src/App.vue
@@ -3,53 +3,65 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  
-<router-view/>
-<!-- 
-<div v-for="shoe in shoes"> </div> -->
+  <router-view/>
 
-<h1>SHOE SIZE</h1>
+<h1>Products</h1>
 
-<p>the shoe size is {{size}}</p>
-
+<p>{{shoes}}</p>
 
 </template>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-</style>
+nav {
+  padding: 30px;
+}
 
-<script>
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-// export default {
-
-// data(){
-
-//   return{
-
-//     shoes:[]
-
-//   }
-
-// },
-
-//   mounted(){
-
-//     fetch('/data/db.json')
-//     .then(res => res.json())
-//     .then(data => this.shoes = data)
-//   }
-
-// }
-
-export default {
-
-  props:["size"]
-
-
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 
 
+</style>
 
+
+
+
+<script>
+
+export default {
+
+data(){
+
+  return{
+
+    shoes:[]
+
+  }
+
+},
+
+  mounted(){
+
+    fetch('https://jason-barron07.github.io/Vue-prod/vue-products/data/db.json')
+    .then(res => res.json())
+    .then(data => this.shoes = data)
+    .catch(err => console.log(this.shoes))
+  }
+}
 
 </script>
+
+//vuex , redirectives, acious
