8764ff9ccd3201cddb9f8824b9c92f2bb05c429d
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-06-26 14:00:35 +0200

    vue

---
+++ vue-products/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  
+<router-view/>
+<!-- 
+<div v-for="shoe in shoes"> </div> -->
+
+<h1>SHOE SIZE</h1>
+
+<p>the shoe size is {{size}}</p>
+
+
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+// export default {
+
+// data(){
+
+//   return{
+
+//     shoes:[]
+
+//   }
+
+// },
+
+//   mounted(){
+
+//     fetch('/data/db.json')
+//     .then(res => res.json())
+//     .then(data => this.shoes = data)
+//   }
+
+// }
+
+export default {
+
+  props:["size"]
+
+
+}
+
+
+
+
+</script>
